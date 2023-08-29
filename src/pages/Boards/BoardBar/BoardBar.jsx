c3483f295f13d38b9bcb0346856c7c73339df107
hongvy0707 <hongvy0707@gmail.com>
2023-08-29 23:59:00 +0700

    initialize app

---
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -0,0 +1,110 @@
+import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Tooltip } from '@mui/material'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import { capitalizeFirstLetter } from '~/utils/formatters'
+
+const MENU_STYLES = {
+  color: 'white',
+  bgcolor: 'transparent',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '.MuiSvgIcon-root': {
+    color: 'white'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
+
+function BoardBar({ board }) {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        paddingX: 2,
+        overflowX: 'auto',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2')
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip sx={MENU_STYLES} icon={<DashboardIcon />} label={board?.title} clickable />
+
+        <Chip sx={MENU_STYLES} icon={<VpnLockIcon />} label={capitalizeFirstLetter(board?.type)} clickable />
+
+        <Chip sx={MENU_STYLES} icon={<AddToDriveIcon />} label='Add To Google Drive' clickable />
+
+        <Chip sx={MENU_STYLES} icon={<BoltIcon />} label='Automation' clickable />
+
+        <Chip sx={MENU_STYLES} icon={<FilterListIcon />} label='Filters' clickable />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button
+          variant='outlined'
+          startIcon={<PersonAddIcon />}
+          sx={{ color: 'white', borderColor: 'white', '&:hover': { borderColor: 'white' } }}
+        >
+          Invite
+        </Button>
+
+        <AvatarGroup
+          max={3}
+          sx={{
+            gap: '10px',
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0be' }
+            }
+          }}
+        >
+          <Tooltip title='Mark Zuckerberg'>
+            <Avatar
+              alt='Mark Zuckerberg'
+              src='https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg'
+            />
+          </Tooltip>
+          <Tooltip title='Elon Musk'>
+            <Avatar
+              alt='Elon Musk'
+              src='https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg'
+            />
+          </Tooltip>
+          <Tooltip title=''>
+            <Avatar
+              alt=''
+              src=''
+            />
+          </Tooltip>
+          <Tooltip title=''>
+            <Avatar
+              alt=''
+              src=''
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+    </Box>
+  )
+}
+
+export default BoardBar
